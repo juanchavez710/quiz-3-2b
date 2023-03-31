797896ad29f885428fbbf84956b93dd4256d3fc3
jaechizzle4 <112498033+juanchavez710@users.noreply.github.com>
2023-03-30 21:46:25 -0500

    hi

--- library.cpp
+++ library.cpp
@@ -23,7 +23,11 @@ condition_variable checkout_ready;
 */
 void books_checkout(int quantity)
 {
-
+    unique_lock<mutex> lock(mut);
+    while (books_available < quantity) {
+        checkout_ready.wait(lock);
+    }
+    books_available -= quantity;
 }
 
 /* TODO:
@@ -34,7 +38,9 @@ void books_checkout(int quantity)
  */
 void books_checkin(int quantity)
 {
-
+    unique_lock<mutex> lock(mut);
+    books_available += quantity;
+    checkout_ready.notify_all();
 }
 
 /**
@@ -61,8 +67,13 @@ int main()
     thread *student_threads = new thread[6];
 
     /*TODO: Create the six student threads that will use the function (manager) for a respective number of books (cart[i])*/
-
+    for (int i = 0; i < 6; i++) {
+        student_threads[i] = thread(manager, i, cart[i]);
+    }
     /*TODO: Wait for all the six threads*/
+    for (int i = 0; i < 6; i++) {
+        student_threads[i].join();
+    }
 
     delete[] student_threads;
     return 0;
